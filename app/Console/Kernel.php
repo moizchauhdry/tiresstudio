5d716f09981803951ef8e856e8232662fda1a613
noaman-hashmi <76763834+noaman-hashmi@users.noreply.github.com>
2022-04-03 01:36:53 +0500

    changes in import controller and Jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         Log::info('A Hit by CRONJOB at '.Carbon::now());
+        if (!$this->osProcessIsRunning('queue:work')) {
+            Log::info('queue:work');
+            $schedule->command('queue:work  --timeout=3600')->everyMinute();
+        }
         $schedule->command('import:wheels')->dailyAt('02:35')->withoutOverlapping()->onSuccess(function (){ \Log::info('import:wheels Successes');})->onFailure(function (){ \Log::info('import:wheels Faileddaily');});
         $schedule->command('fetch:wheels')->weekly()->withoutOverlapping()->onSuccess(function (){ \Log::info('fetch:wheels Successes');})->onFailure(function (){ \Log::info('fetch:wheels Faileddaily');});
         $schedule->command('fetch:tires')->weekly()->withoutOverlapping()->onSuccess(function (){ \Log::info('fetch:tires Successes');})->onFailure(function (){ \Log::info('fetch:tires Faileddaily');});
@@ -44,5 +48,22 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    protected function osProcessIsRunning($needle)
+    {
+        // get process status. the "-ww"-option is important to get the full output!
+        exec('ps aux -ww', $process_status);
+
+        // search $needle in process status
+        $result = array_filter($process_status, function($var) use ($needle) {
+            return strpos($var, $needle);
+        });
+
+        // if the result is not empty, the needle exists in running processes
+        if (!empty($result)) {
+            return true;
+        }
+        return false;
     }
 }
