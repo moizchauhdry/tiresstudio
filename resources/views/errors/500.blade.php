677b7fdf532d7eb61cc254c1022e076fa9ec57de
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2022-04-15 00:03:30 +0500

    Frontend changes

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.frontend')
 @section('content')
-<section class="text-center mt-4">
-    <h2 style="color: red">The request could not be satisfied.</h2>
-    <h5>You don't have permission to access this. There might be configuration error. Try again later, or contact the
-        support.</h5>
-    <a href="{{url()->previous()}}" class="btn btn-dark mt-4">Return Back</a>
+<section class="text-center" style="padding:100px">
+    <h1 style="color: red">The request could not be satisfied.</h1>
+    <h4>You don't have permission to access this. There might be configuration error. <br> Try again later, or contact
+        the
+        support.</h4>
+    <a href="{{route('frontend.pages.index')}}" class="btn btn-default">Return Home</a>
 </section>
 @endsection
