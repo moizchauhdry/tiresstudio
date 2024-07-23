2137787b5005525411c117ce102942f87430aaf1
noaman-hashmi <76763834+noaman-hashmi@users.noreply.github.com>
2024-07-24 00:52:49 +0500

    shell fix

--- public/shell-scripts/cron-delete-script.sh
+++ public/shell-scripts/cron-delete-script.sh
@@ -3,22 +3,22 @@
 # Backup the crontab
 crontab -l > crontab.bak
 
-# Regular expression to match 5-minute cron jobs
+# Regular expression to match 5-minute cron jobs (basic)
 five_min_cron_regex='^\s*\*/5\s'
 
-# Temporary file for modified crontab
-temp_crontab=""
+# Temporary array to store lines to be kept
+keep_lines=()
 
 while read line; do
   if [[ ! $line =~ $five_min_cron_regex ]]; then
-    echo "$line" >> $temp_crontab
+    keep_lines+=("$line")
   fi
 done < crontab.bak
 
 # Install the modified crontab
-crontab < $temp_crontab
+printf "%s\n" "${keep_lines[@]}" | crontab -
 
-# Remove temporary files
-rm crontab.bak $temp_crontab
+# Remove temporary file
+rm crontab.bak
 
 echo "cron removed"
